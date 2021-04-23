a0fc38dfd288065e7a1d700959669788c4eb9c63
ishiwatm <ishiwatm@gmail.com>
2021-04-23 15:31:17 +0900

    empty android project first commit.

---
+++ app/src/main/java/com/example/get_data_from_jetson_ble_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.get_data_from_jetson_ble_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
